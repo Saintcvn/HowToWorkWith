ccaf6cc559fae53b2bb2c2b66ff4c277197bf761
Saintcvn <cvn1426@gmail.com>
2023-07-05 20:06:00 +0900

    *

---
+++ utility.py
@@ -0,0 +1,12 @@
+
+def add(alpha: int, beta: int) -> int:
+    return alpha + beta
+
+def subtract(alpha: int, beta: int)-> int:
+    return alpha - beta
+
+def multiply(alpha: int, beta: int) -> int:
+    return alpha * beta
+    
+def divide(alpha: int, beta:int) -> tuple:
+    return alpha // beta, alpha % beta
